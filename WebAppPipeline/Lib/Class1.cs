1c23b68d802d72e0504c996733bb1aded5c11cf4
Sergio Fontes <sergio@computencial.com>
2022-05-05 19:38:30 +0100

    Introdução de lib e test project

---
+++ WebAppPipeline/Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Exemplo
+    {
+        public static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ WebAppPipeline/UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Lib;
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Soma()
+        {
+            int resultado = Exemplo.Soma(2, 2);
+
+            Assert.Equal(4, resultado);
+
+        }
+    }
+}
